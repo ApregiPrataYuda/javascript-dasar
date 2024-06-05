245aab67bc3b2174c02337a7d7b1ee564cf62867
ApregiPrataYuda <Apregipratayda@gmail.com>
2024-06-05 14:20:01 +0700

    add

--- class.mjs
+++ class.mjs
@@ -1,3 +1,20 @@
+// Kelas
+// Kelas adalah jenis fungsi, namun alih-alih menggunakan kata kunci fungsi untuk memulainya, kita menggunakan kata kunci kelas,
+//  dan properti ditetapkan di dalam metode konstruktor(). Kelas adalah templat untuk membuat objek, dan mereka merangkum semua data dan fungsionalitas suatu objek di satu tempat.
+//  Kelas digunakan dalam bahasa Pemrograman Berorientasi Objek (OOP), seperti JavaScript, untuk membuat objek. Dalam JavaScript,
+//   kelas adalah warga kelas satu, yang berarti mereka diperlakukan seperti variabel lainnya, dan dapat diberi nilai apa pun, termasuk fungsi dan objek.
+
+// Membuat Kelas
+// Untuk membuat kelas, gunakan kata kunci class diikuti dengan nama kelas. Nama kelas harus menggunakan huruf kapital. 
+// Metode konstruktor adalah metode khusus untuk membuat dan menginisialisasi objek yang dibuat dengan suatu kelas.
+// Hanya ada satu metode khusus dengan nama “konstruktor” di sebuah kelas.
+//  Konstruktor dapat menggunakan kata kunci super untuk memanggil konstruktor kelas induk.
+
+// Membuat Instance Kelas
+// Untuk membuat instance kelas, gunakan kata kunci baru diikuti dengan nama kelas. 
+// Ini akan membuat objek baru. Kata kunci new membuat objek baru, 
+// dan kemudian memanggil metode konstruktor pada objek tersebut.
+
 //class js
 class person{
     constructor(names) {
@@ -15,7 +32,11 @@ class person{
 
 
 
-
+//    Metode Statis
+//    Metode statis adalah metode yang dikaitkan dengan kelas,
+//     tetapi tidak dengan objek tertentu di kelas tersebut. Untuk membuat metode statis,
+//     gunakan kata kunci statis sebelum nama metode. Metode statis berguna untuk fungsi utilitas, 
+//     seperti fungsi untuk membuat atau mengkloning objek.
 
 //    class Static Methods
    class Person {
@@ -40,7 +61,11 @@ class person{
 
 
 
-
+//   Warisan Kelas
+//   Warisan kelas adalah cara untuk memperluas fungsionalitas suatu kelas dengan membuat kelas baru yang mewarisi dari kelas aslinya.
+//    Kelas baru mewarisi semua metode dan properti dari kelas asli,
+//    dan juga dapat memiliki metode dan properti tambahannya sendiri. 
+//    Untuk membuat kelas yang mewarisi kelas lain, gunakan kata kunci extends.
 
 
   //class pewarisan inheritance
